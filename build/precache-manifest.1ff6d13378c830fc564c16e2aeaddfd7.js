6a0dfa954223376297793b6a1ed06fece8d79ebc
mukul <mukulgautam5200@gmail.com>
2022-04-10 11:04:06 +0530

    project complete

---
+++ build/precache-manifest.1ff6d13378c830fc564c16e2aeaddfd7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca2380a0c8949b12dd46b28a17429b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "37537b6f35e289a9816d",
+    "url": "/static/js/main.91246512.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
